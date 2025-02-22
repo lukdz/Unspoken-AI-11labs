096e8617c4efed62e182e39af860708e7cf9ffcb
lukasz.dzwoniarek <lukasz.dzwoniarek@codewave.pl>
2025-02-23 00:12:27 +0100

    Refactor layout structure to improve HTML semantics and organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,26 +11,23 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" className={"h-full w-full"}>
-        <AnalyticsProvider>
-        <div className="flex flex-col flex-grow w-full items-center justify-center sm:px-4">
-            <nav
-                className={
-                    "sm:fixed w-full top-0 left-0 grid grid-cols-2 py-4 px-8"
-                }
-            >
-                <div className={"flex"}>
-                    <Link href={"/"} prefetch={true}>
-                        <p className="text-lg font-bold">Unspoken AI</p>
-                    </Link>
-                </div>
-            </nav>
-            <div className="flex items-center mx-auto">
-                {children}
-            </div>
-            <BackgroundWave/>
-        </div>
-        </AnalyticsProvider>
-        </body>
+            <body>
+                <AnalyticsProvider>
+                    <div className="flex flex-col flex-grow w-full items-center justify-center sm:px-4">
+                        <nav className={"sm:fixed w-full top-0 left-0 grid grid-cols-2 py-4 px-8"}>
+                            <div className={"flex"}>
+                                <Link href={"/"} prefetch={true}>
+                                    <p className="text-lg font-bold">Unspoken AI</p>
+                                </Link>
+                            </div>
+                        </nav>
+                        <div className="flex items-center mx-auto">
+                            {children}
+                        </div>
+                        <BackgroundWave/>
+                    </div>
+                </AnalyticsProvider>
+            </body>
         </html>
     );
 }
